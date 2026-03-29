9483e5e60cd7e92a6de5bfaebbff87d5690c43fb
Larry Thuku <larrythuku18@gmail.com>
2026-03-29 13:54:56 +0300

    First javascript assignment

--- inventoryManagement.js
+++ inventoryManagement.js
@@ -1,11 +1,21 @@
 // Write your code here
+const products = ['Laptop', 'Phone', 'Headphones', 'Monitor', 'Keyboard'];
 
-
+function logFirstProduct(products) {
+  console.log(products[0]);
+}
+function updateProductName(products) {
+  products.push('Mouse');
+  console.log(products);
+}
+function removeLastProduct(products) {
+  products.pop();
+  console.log(products);
+}
 
 // Export the necessary parts for testing
 module.exports = {
   logFirstProduct: typeof logFirstProduct !== 'undefined' ? logFirstProduct : undefined,
-  addProduct: typeof addProduct !== 'undefined' ? addProduct : undefined,
   updateProductName: typeof updateProductName !== 'undefined' ? updateProductName : undefined,
   removeLastProduct: typeof removeLastProduct !== 'undefined' ? removeLastProduct : undefined,
   products
